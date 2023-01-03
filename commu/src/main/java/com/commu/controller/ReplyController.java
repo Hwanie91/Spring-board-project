ec194e51d55e0336c7b7e795a8d179123a788fbd
Hwanie91 <jh27hot@gmail.com>
2023-01-03 11:12:23 +0900

    Spring_board project - 댓글 기능 테스트(REST API)

--- commu/src/main/java/com/commu/controller/ReplyController.java
+++ commu/src/main/java/com/commu/controller/ReplyController.java
@@ -1,23 +1,32 @@
 package com.commu.controller;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.commu.domain.Criteria;
 import com.commu.domain.ReplyVO;
 import com.commu.service.ReplyService;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
-@RestController
+
 @Log4j
 @AllArgsConstructor
 @RequestMapping("/replies")
+@RestController
 public class ReplyController {
 	
 	private ReplyService service;
@@ -30,6 +39,50 @@ public class ReplyController {
 		
 		return insertCount == 1? new ResponseEntity<>("success", HttpStatus.OK)
 				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		// ResponseEntity : 웹페이지 생성(상태코드, 헤더, 응답, 데이터)
+		// 3항 연산자 사용
+		// HttpStatus 페이지 상태 전달, 정상처리되면 정상처리의 status 전달하고 아니면 오류 status 전달
+		
 	}
+	
+	@GetMapping(value="/pages/{bno}/{page}", produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<List<ReplyVO>> getList(@PathVariable("page") int page, 
+													@PathVariable("bno") Long bno) { 
+		// @PathVariable : url로 넘겨받은 값 이용
+		log.info("getList...");
+		Criteria cri = new Criteria(page, 10);
+		log.info(cri);
+		
+		return new ResponseEntity<>(service.getList(cri, bno), HttpStatus.OK);
+	}
+	
+	@GetMapping(value="/{rno}", produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<ReplyVO> get(@PathVariable("rno") Long rno) {
+		log.info("get: " + rno);
+		
+		return new ResponseEntity<>(service.get(rno), HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value="/{rno}", produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+		log.info("remove" + rno);
+		
+		return service.remove(rno) == 1? new ResponseEntity<>("success", HttpStatus.OK)
+											: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@RequestMapping(method= {RequestMethod.PUT, RequestMethod.PATCH}, value="/{rno}", 
+						consumes = "application/json", produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> modify(@RequestBody ReplyVO vo, @PathVariable("rno") Long rno) {
+		vo.setRno(rno);
+		log.info("rno : " + rno);
+		log.info("modify : " + vo);
+		
+		return service.modify(vo) == 1? new ResponseEntity<>("success", HttpStatus.OK)
+										: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	
 	
 }
