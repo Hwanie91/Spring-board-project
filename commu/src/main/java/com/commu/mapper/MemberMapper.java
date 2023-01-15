8bdd82a3bdfbbc1b6d7c98ac9bab8ef540dc95d2
Hwanie91 <jh27hot@gmail.com>
2023-01-15 23:53:19 +0900

    Create MemberMapper.java

---
+++ commu/src/main/java/com/commu/mapper/MemberMapper.java
@@ -0,0 +1,10 @@
+package com.commu.mapper;
+
+import com.commu.domain.MemberVO;
+
+public interface MemberMapper {
+	
+	public MemberVO read(String userid);
+	// 사용자가 아이디를 입력하면, 그에 해당하는 계정 정보를 디비에서 추출
+	
+}
